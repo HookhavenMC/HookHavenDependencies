da29f5db34e5237200199ead1b8a6726e26b5c2c
Rosapetals <52610978+Rosapetals@users.noreply.github.com>
2023-08-26 16:10:01 -0500

    dependencies

---
+++ src/main/java/net/hookhaven/HookHavenDependencies.kt
@@ -0,0 +1,14 @@
+package net.hookhaven
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class HookHavenDependencies : JavaPlugin() {
+    override fun onEnable() {
+
+        println("[HOOKHAVEN DEPENDENCIES] Loaded.")
+
+    }
+
+    override fun onDisable() {
+    }
+}
